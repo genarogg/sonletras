db50f526ee97d4131efaa46d78433d3c556d3a1f
genarogg <gena_gonzalez15@hotmail.com>
2020-01-13 15:27:51 -0400

    inicializando los state y creado el formulario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React , { useState, useEffect, Fragment } from 'react';
 
 function App(){
-  return <p>Hola</p>
+
+  /* Utilizar useState con 3 states */
+  const [ artista, agregarArtista ] = useState("");
+  const [ letra, agregarLetra ] = useState([]);
+  const [ info, agregarInfo ] = useState({});
+
+  return (
+    <Fragment>
+      
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react"
+
+function Formulario(){
+    return <p>desde Fromulario</p>
+}
+
+export default Formulario;
